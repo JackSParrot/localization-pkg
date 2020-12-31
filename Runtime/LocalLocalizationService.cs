e6173392481710c6bffc5e3f507c8d451c65871a
JackSParrot <franciscoruizpons@gmail.com>
2020-12-31 10:03:19 +0100

    Update LocalLocalizationService.cs

--- Runtime/LocalLocalizationService.cs
+++ Runtime/LocalLocalizationService.cs
@@ -6,15 +6,17 @@ namespace JackSParrot.Services.Localization
 {
     public interface ILocalizationService : System.IDisposable
     {
+        event System.Action OnLocalizationChanged;
         bool Initialized { get; }
         void Initialize(System.Action onFinished);
+        void SetLanguage(SystemLanguage newLanguage);
         string GetLocalizedString(string key);
     }
 
     public class LocalLocalizationService : ILocalizationService
     {
-        SystemLanguage _defaultLanguage;
-        Dictionary<SystemLanguage, string> LocalizationFiles = new Dictionary<SystemLanguage, string>
+        public event System.Action OnLocalizationChanged = delegate() { };
+        private Dictionary<SystemLanguage, string> LocalizationFiles = new Dictionary<SystemLanguage, string>
         {
             {SystemLanguage.Spanish, "es"},
             {SystemLanguage.English, "en"},
@@ -23,23 +25,33 @@ namespace JackSParrot.Services.Localization
             {SystemLanguage.Italian, "it"}
         };
 
-        Localization _currentLocalization;
+        private SystemLanguage _defaultLanguage;
+        private SystemLanguage _chosenLanguage;
+        private Localization _currentLocalization;
         public bool Initialized { get; private set; }
 
         public LocalLocalizationService(SystemLanguage defaultLanguage = SystemLanguage.English)
         {
             _defaultLanguage = defaultLanguage;
+            _chosenLanguage = Application.systemLanguage;
             _currentLocalization = new Localization();
             Initialized = false;
         }
 
-        public void Initialize(System.Action onFinishedLoading)
+        public void SetLanguage(SystemLanguage newLanguage)
+        {
+            _chosenLanguage = newLanguage;
+            Initialize();
+        }
+
+        public void Initialize(System.Action onFinishedLoading = null)
         {
             Initialized = false;
-            var language = Application.systemLanguage;
+            var language = _chosenLanguage;
             if(!LocalizationFiles.ContainsKey(language))
             {
                 language = _defaultLanguage;
+                SharedServices.GetService<ICustomLogger>()?.LogError("Chosen language is not available in localization files");
             }
             string fileName = "localization_" + LocalizationFiles[language];
             TextAsset content = Resources.Load<TextAsset>(fileName);
@@ -51,13 +63,21 @@ namespace JackSParrot.Services.Localization
                 if(content == null)
                 {
                     SharedServices.GetService<ICustomLogger>()?.LogError("Can not load the default localization file.");
-                    onFinishedLoading();
+                    onFinishedLoading?.Invoke();
                     return;
                 }
             }
-            _currentLocalization.FromJSON(JSON.JSON.LoadString(content.text));
+            try
+            {
+                _currentLocalization.FromJSON(JSON.JSON.LoadString(content.text));
+            }
+            catch(System.Exception)
+            {
+                SharedServices.GetService<ICustomLogger>()?.LogError("Error parsing localization file: " + fileName);
+            }
             Initialized = true;
-            onFinishedLoading();
+            OnLocalizationChanged();
+            onFinishedLoading?.Invoke();
         }
 
         public string GetLocalizedString(string key)
